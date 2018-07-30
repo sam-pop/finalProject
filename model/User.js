e42e51152c858f4beb6eef50950716977a80d33e
sam-pop <poplovitch.samuel@gmail.com>
2018-07-30 14:23:07 -0400

    passport

--- model/User.js
+++ model/User.js
@@ -1,25 +1,44 @@
 const bcrypt = require("bcrypt-nodejs");
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
-  profile_pic: { type: String, required: true },
-  location: [Number, [Number]]
+  email      : { type: String,  required: true, index: { unique: true } },
+  password   : { type: String,  required: true },
+  name       : { type: String,  required: true },
+  profile_pic: { type: String,  required: true },
+  location   : [       Number, [Number  ]]
 });
 
-const User = mongoose.model("User", userSchema);
+UserSchema.pre("save", function(next) {
+  var user = this;
 
-// User protoype method to check if our user password is valid
-User.prototype.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.password);
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
 };
 
-// pre table creation hook to hash the user password
-User.hook("beforeCreate", function(user) {
-  user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(12), null);
-});
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
