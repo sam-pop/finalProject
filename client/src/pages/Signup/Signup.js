82d80ee4260adbe3d154d1b3e2586d5b5d20cfc1
nchenari <nimachenari@gmail.com>
2018-07-30 15:19:45 -0400

    added code for Login and Signup pages

---
+++ client/src/pages/Signup/Signup.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+import { Col, Row, Container } from "../../components/Grid";
+import { Input, FormBtn } from "../../components/Form";
+
+class Signup extends Component {
+    state = {
+        email: "",
+        password: "",
+        name: "",
+        location: ""
+    };
+
+    componentDidMount() {
+
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        if (this.state.name && this.state.password 
+            && this.state.name && this.state.location) {
+                // api call to save user
+        }
+    };
+
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col size="md-12">
+                        <Jumbotron>
+                            <h1>Signup!</h1>
+                        </Jumbotron>
+                        <form>
+                            <Input
+                                value={this.state.email}
+                                onChange={this.handleInputChange}
+                                name="email"
+                                placeholder="email (required)"
+                            />
+                            <Input
+                                value={this.state.password}
+                                onChange={this.handleInputChange}
+                                name="password"
+                                placeholder="password (required)"
+                            />
+                            <Input
+                                value={this.state.name}
+                                onChange={this.handleInputChange}
+                                name="name"
+                                placeholder="name"
+                            />
+                            <Input
+                                value={this.state.location}
+                                onChange={this.handleInputChange}
+                                name="location"
+                                placeholder="location (state/province, Country)"
+                            />
+                            <FormBtn
+                                disabled={!(this.state.email && this.state.password && this.state.name && this.state.location)}
+                                onClick={this.handleFormSubmit}
+                            >
+                                Signup
+                            </FormBtn>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default Signup;
