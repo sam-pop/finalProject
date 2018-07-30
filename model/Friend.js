bceaac1f5e54d69e63f87b85a0a0da33bff9c57c
sam-pop <poplovitch.samuel@gmail.com>
2018-07-29 21:09:53 -0400

    model update

---
+++ model/Friend.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const friendSchema = new Schema({
+  name        : { type: String, required: true },
+  profile_pic : { type: String, required: true },
+  location    : [ Number, Number],
+  reminder    : Date ,
+  note        : String
+});
+
+const Friend = mongoose.model("Friend", friendSchema);
+
+module.exports = Friend;
---
+++ model/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email      : { type: String, required: true },
+  password   : { type: String, required: true },
+  name       : { type: String, required: true },
+  profile_pic: { type: String, required: true },
+  location   : [Number, Number]
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
