e8f864225573984a2ea8d974b3548348494e6819
mecaputy <martina.e.caputy@gmail.com>
2018-07-31 11:58:43 -0400

    leaflet fix

--- client/src/components/MapCard/MapCard.js
+++ client/src/components/MapCard/MapCard.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { render } from "react-dom";
+// import { render } from "react-dom";
+// import { Container } from '../Grid';
 import { Map, Marker, TileLayer } from "react-leaflet";
 import "./MapCard.css";
 
-const position = [29.131109, -36.311774];
-const MapCard = () => {
-  return (
-    <Map center={position} zoom={2}>
+// const position = [29.131109, -36.311774];
+class MapCard extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            lat: 29.131109,
+            lng: -36.311774,
+            zoom: 2
+        }
+    }
+    render() {
+        const position = [this.state.lat, this.state.lng];
+
+  return (   
+      <div className="leaflet-container">
+    <Map center={position} zoom={this.state.zoom} style={{height: "100vh"}}>
       <TileLayer
         url="https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}"
         attribution="Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery © <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>"
@@ -15,9 +28,11 @@ const MapCard = () => {
       />
       <Marker position={position} />
     </Map>
+    </div>
   );
 };
+}
 
-// render(MapCard, document.getElementById('map-container'));
+// render(<MapCard />);
 
 export default MapCard;
