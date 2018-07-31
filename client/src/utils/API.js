e1bae956e4f7387c4b96560a0f770fadf88a74aa
sam-pop <poplovitch.samuel@gmail.com>
2018-07-31 14:15:59 -0400

    API.js update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,50 +1,83 @@
 import axios from "axios";
 
 const GEOCODE_API_KEY = "";
-const GEOCODE_URL = 
-    "https://maps.googleapis.com/maps/api/geocode/json?key=" 
-    + GEOCODE_API_KEY + "&address=";
+const GEOCODE_URL =
+  "https://maps.googleapis.com/maps/api/geocode/json?key=" +
+  GEOCODE_API_KEY +
+  "&address=";
 
 const DARKSKY_API_KEY = "";
-const DARKSKY_URL = 
-    "https;//api.darksky.net/forecast/" 
-    + DARKSKY_API_KEY; // add /<latitude>,<longitude>
-
+const DARKSKY_URL = "https;//api.darksky.net/forecast/" + DARKSKY_API_KEY; // add /<latitude>,<longitude>
 
 const NEWS_API_KEY = "";
-const NEWS_URL = 
-    "https://newsapi.org/v2/everything?sources=reuters&apiKey=" 
-    + NEWS_API_KEY; // add q=<coutnry> 
+const NEWS_URL =
+  "https://newsapi.org/v2/everything?sources=reuters&apiKey=" + NEWS_API_KEY; // add q=<coutnry>
 
 export default {
-    // get weather for location
-    getWeather: function(location) {
+  // get weather for location
+  getWeather: function(location) {
+    axios
+      .get(GEOCODE_URL + location)
+      .then(response => {
+        if (response.data.status === "ZERO_RESULTS") {
+          throw new Error("Unable to find that address.");
+        }
 
-        axios.get(GEOCODE_URL + location)
-            .then((response) => {
+        let latitude = response.data.results[0].geometry.location.lat;
+        let longitude = response.data.results[0].geometry.location.lng;
 
-                if (response.data.status === 'ZERO_RESULTS') {
-                    throw new Error("Unable to find that address.");
-                }
+        const weatherURL = DARKSKY_URL + `/${latitude},${longitude}`;
 
-                let latitude = response.data.results[0].geometry.location.lat;
-                let longitude = response.data.results[0].geometry.location.lng;
+        return axios.get(weatherURL);
+      })
+      .catch(e => {
+        if (e.code === "ENOTFOUND") {
+          console.log("Unable to connect ot API servers.");
+        } else {
+          console.log(e.message);
+        }
+      });
+  },
+  // get news for location
+  getNews: function(location) {
+    return axios.get();
+  },
 
-                const weatherURL = DARKSKY_URL + `/${latitude},${longitude}`
+  // -------- mongo database calls ---------
 
-                return axios.get(weatherURL);
-            }).catch((e) => {
+  //FRIEND API CALLS
+  // Gets all friends
+  getFriends: function() {
+    return axios.get("/api/friends");
+  },
+  // Gets the friend with the given id
+  getFriend: function(id) {
+    return axios.get("/api/friends/" + id);
+  },
+  // Deletes the friend with the given id
+  deleteFriend: function(id) {
+    return axios.delete("/api/friends/" + id);
+  },
+  // Saves a book to the database
+  saveFriend: function(friendData) {
+    return axios.post("/api/friends", friendData);
+  },
 
-                if (e.code === 'ENOTFOUND') {
-                    console.log('Unable to connect ot API servers.');
-                } else {
-                    console.log(e.message);
-                }
-            });
-    },
-    // get news for location
-    getNews: function(location) {
-        return axios.get();
-    }
-    // -------- mongo database calls ---------
+  //USER API CALLS
+  // Gets all users
+  getUsers: function() {
+    return axios.get("/api/users");
+  },
+  // Gets the friend with the given id
+  getUser: function(id) {
+    return axios.get("/api/users/" + id);
+  },
+  // Deletes the friend with the given id
+  deleteUser: function(id) {
+    return axios.delete("/api/users/" + id);
+  },
+  // Saves a book to the database
+  saveUser: function(userData) {
+    return axios.post("/api/users", userData);
+  }
 };
