8f6eec253d55c86ebad2170479bb62fa5e06f860
mecaputy <martina.e.caputy@gmail.com>
2018-08-02 11:51:38 -0400

    CSS

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,7 +1,8 @@
 import React from "react";
+import './Form.css';
 
 export const FormBtn = props => (
-    <button {...props} style={{ float: "right", marginBotton: 10 }} className="btn btn-success">
+    <button {...props} style={{ float: "left", marginBottom: 50, marginLeft: 100}} className="btn btn-success">
         {props.children}
     </button>
 );
