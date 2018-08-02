929ec4b56963b92805861268580995beb7348f98
sam-pop <poplovitch.samuel@gmail.com>
2018-08-02 11:33:24 -0400

    avatar remove for deployment

--- client/src/components/ProfilePhoto/ProfilePhoto.js
+++ client/src/components/ProfilePhoto/ProfilePhoto.js
@@ -1,12 +1,8 @@
 import React from "react";
-import UserAvatar from "react-user-avatar";
+// import UserAvatar from "react-user-avatar";
 
 const ProfilePhoto = props => (
-    <UserAvatar 
-        size="70"
-        name={props.name}
-        src={props.profile_pic}
-    />
+    <img src='https://cdn4.iconfinder.com/data/icons/user-avatar-flat-icons/512/User_Avatar-31-512.png' width='70px'/>
 );
 
 export default ProfilePhoto;
